2a5ea5f64ad455940c4d6a9add4b58c9f63ca85c
muhammadsodik <ilmyolida77@gmail.com>
2026-04-01 17:39:09 +0600

    router.dart

--- traceit/lib/core/router.dart
+++ traceit/lib/core/router.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:traceit/features/main_screen.dart'; // Fayl yo'li sizda qanday bo'lsa shunday yozing
 // GoRouter - bu ilovadagi barcha yo'llarni boshqaruvchi klass
-final AppRouter = GoRouter(
+final appRouter = GoRouter(
   initialLocation: '/', // Ilova ochilganda birinchi bo'lib ko'rsatiladigan manzil
   routes: [
     GoRoute(
